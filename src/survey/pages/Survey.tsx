2990e4b7bf0b1cc628772c48cc93de4c54c5f57d
teocchiii <u202211212@upc.edu.pe>
2025-01-24 01:18:26 -0500

    feat(survey): component created

---
+++ src/survey/pages/Survey.tsx
@@ -0,0 +1,13 @@
+import "./Survey.css";
+
+
+export function Survey(){
+    return(
+        <>
+            <div>
+                <h2>Survey funciona</h2>
+            </div>
+
+        </>
+    )
+}
