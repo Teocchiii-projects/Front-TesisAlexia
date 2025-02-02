0f7b506348aad4cc6546d9c40d7b94b05818b7fe
teocchiii <u202211212@upc.edu.pe>
2025-02-02 02:08:14 -0500

    feat(ACE): feat added.

---
+++ src/survey/pages/ACE.tsx
@@ -0,0 +1,9 @@
+import "./Pages.css"
+
+export function ACE(){
+    return(
+        <>
+        <h2>Cuestionario para Adultos de Experiencias Adversas en la infancia (ACE)</h2>
+        </>
+    )
+}
