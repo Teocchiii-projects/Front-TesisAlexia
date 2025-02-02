13d933ad60057757b436e789b40e9529454f9a32
teocchiii <u202211212@upc.edu.pe>
2025-02-02 02:07:57 -0500

    feat(FiRST): feat added.

---
+++ src/survey/pages/FiRST.tsx
@@ -0,0 +1,12 @@
+import "./Pages.css"
+
+export function FiRST (){
+    return(
+        <>
+        <h2>Síntomas de fibromialgia (FiRST versión en español)</h2>
+            <form>
+
+            </form>
+        </>
+    )
+}
